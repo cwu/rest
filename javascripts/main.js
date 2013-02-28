8414e14bb0e82048ec9283644b1ae15833f4f4eb
Chris Wu <me@chriswu.me>
2013-02-27 21:57:18 -0500

    smooth scrolling

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,26 @@
-console.log('This would be the main JS file.');
+$(function() {
+  var navBottom = $('#nav_wrap').offset().top + $('#nav_wrap').height()
+  if ($(window).scrollTop() > navBottom) {
+    $('#nav_wrap').addClass('sticky');
+  }
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > navBottom) {
+      $('#nav_wrap').addClass('sticky');
+    } else {
+      $('#nav_wrap').removeClass('sticky');
+    }
+  });
+  $('a[href*=#]:not([href=#])').on('click', function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        || location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
